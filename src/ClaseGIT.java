f2b9d2c495a0e2b770307dae8cc6f3fbcedda458
Mati <Matipeer123@gmail.com>
2017-06-10 20:20:15 -0300

    Se realiza lo pedido

--- src/ClaseGIT.java
+++ src/ClaseGIT.java
@@ -12,7 +12,7 @@ public class ClaseGIT {
 		//3. Que su compañero se lo descargue
 		System.out.println("Hola Clase!!!");
 		
-		System.out.println("Hola Profes!!");
+		System.out.println("Hola Profes!!"); 
 	}
 
 }
