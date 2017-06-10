f9d03a45b2d27be23d893e32b54dd8b926c6ee03
Martin Casanova <Martincasanova933@gmail.com>
2017-06-10 18:45:35 -0300

    Se sube la informacion para que se realice la clase

---
+++ src/ClaseGIT.java
@@ -0,0 +1,17 @@
+
+public class ClaseGIT {
+
+	public static void main(String[] args) {
+		// Este es el proyecto que esta en la nube alojado en gitHub
+		// Muchas gracias por la asistencia de la clase y esperamos que los conocimientos adiquiridos
+		// sean de mucha ayuda para todos.
+		//
+		//INSTRUCCIONES:
+		//1. Agregar una linea que imprima por pantalla "Hola Profes!"
+		//2. Subirlo al repositorio
+		//3. Que su compañero se lo descargue
+		System.out.println("Hola Clase!!!");
+
+	}
+
+}
